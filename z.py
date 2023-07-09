e46c8868f457eb99e9ed125869c9ec596484450e
Irfan <125267993+TheDSBotz@users.noreply.github.com>
2023-07-09 23:04:42 +0530

    Update z.py

--- z.py
+++ z.py
@@ -9,7 +9,7 @@ headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleW
 
 url = "https://www.amazon.com/OnePlus-Unlocked-Android-Smartphone-Charging/dp/B07XWGWPH5/?_encoding=UTF8&pf_rd_p=fd98176f-b79c-4a77-8259-f0fae864b0d6&pd_rd_wg=L7Wki&pf_rd_r=6VHW0KN3CD6RZ0191H0T&content-id=amzn1.sym.fd98176f-b79c-4a77-8259-f0fae864b0d6&pd_rd_w=pAdjg&pd_rd_r=d40bb4c4-5aba-448c-b46b-2899a48ebe3d&ref_=exports-popular-this-season-with-similar-asins"
 
-r = requests.get(url, proxies=proxies, headers=headers)
+r = requests.get(url, headers=headers)
 
 soup = BeautifulSoup(r.text, 'html.parser')
 print(soup.prettify())
