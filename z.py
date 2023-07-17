8877efc299f4bdfc2beb32ad387165cc301fbc9c
Irfan <125267993+TheDSBotz@users.noreply.github.com>
2023-07-17 15:16:16 +0530

    Update z.py

--- z.py
+++ z.py
@@ -1,13 +1,14 @@
 import requests
 from bs4 import BeautifulSoup
+
 proxies = {
-    "http": "http://customer-rcodewithharry:ActcitXccR8xbxs@pr.oxylabs.io:7777",
-    "https": "http://customer-rcodewithharry:ActcitXccR8xbxs@pr.oxylabs.io:7777"
+    "http": "http://RpQQ6Q:bRapWN@168.81.251.135:8000",
+    "https": "http://RpQQ6Q:bRapWN@168.81.251.135:8000"
 }
 
 headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
 
-url = "https://apis-jiovoot.voot.com/recapi/voot/v1/voot-web/watchnow?id=3703198&responseType=common"
+url = "https://httpbin.org/ip"
 
 r = requests.get(url, headers=headers)
 
